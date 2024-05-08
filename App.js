e522908b05863c3893761b6e34ecfd3c3773ade3
Avinash Gupta <guptaavinash92@gmail.com>
2024-05-09 00:24:13 +0100

    Adding Basic UI - Header and Body

---
+++ App.js
@@ -0,0 +1,644 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const data = [
+  {
+    info: {
+      id: "762410",
+      name: "Baap Of Rolls",
+      cloudinaryImageId: "1c911da2b6ebfd3556965dc1ea1133d3",
+      locality: "Begum Bridge Road",
+      areaName: "Meerut Cantt",
+      costForTwo: "₹299 for two",
+      cuisines: ["rolls", "Snacks"],
+      avgRating: 4.3,
+      parentId: "14310",
+      avgRatingString: "4.3",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 43,
+        lastMileTravel: 9.5,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "9.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-05-09 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "ABOVE ₹149",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-963d3769-ee32-4929-a9b2-48ab368f5d0d",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/baap-of-rolls-begum-bridge-road-meerut-cantt-meerut-762410",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "656187",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "Down town",
+      areaName: "Meerut",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.3,
+      parentId: "166",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 52,
+        lastMileTravel: 9.7,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "9.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-05-09 03:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Burger.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Burger.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹159",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-963d3769-ee32-4929-a9b2-48ab368f5d0d",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-down-town-meerut-meerut-656187",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "566523",
+      name: "Faasos' Signature Wraps & Rolls",
+      cloudinaryImageId: "c583ca6ce40b426797a78ae2ac91f2ec",
+      locality: "Sector 9",
+      areaName: "Shastri Nagar",
+      costForTwo: "₹350 for two",
+      cuisines: [
+        "Fast Food",
+        "Snacks",
+        "North Indian",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4.2,
+      parentId: "340366",
+      avgRatingString: "4.2",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 58,
+        lastMileTravel: 13.8,
+        serviceability: "SERVICEABLE",
+        slaString: "55-60 mins",
+        lastMileTravelString: "13.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-05-09 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-963d3769-ee32-4929-a9b2-48ab368f5d0d",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/faasos-signature-wraps-and-rolls-sector-9-shastri-nagar-meerut-566523",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "729479",
+      name: "Pizza Wings",
+      cloudinaryImageId: "413f10d8eb70429253c46fe4136eabed",
+      locality: "garh road",
+      areaName: "Vardhman Plaza",
+      costForTwo: "₹300 for two",
+      cuisines: ["Pizzas", "Pastas", "Italian", "rolls", "Beverages"],
+      avgRating: 4.3,
+      parentId: "159048",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 58,
+        lastMileTravel: 12.8,
+        serviceability: "SERVICEABLE",
+        slaString: "55-60 mins",
+        lastMileTravelString: "12.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-05-09 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-963d3769-ee32-4929-a9b2-48ab368f5d0d",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-wings-garh-road-vardhman-plaza-meerut-729479",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "727373",
+      name: "CP2.0",
+      cloudinaryImageId: "7c0213220506d34cf5864196e9c2cdb9",
+      locality: "Shradha Puri Rd",
+      areaName: "Meerut Cantt",
+      costForTwo: "₹149 for two",
+      cuisines: ["Chinese", "South Indian", "Burgers"],
+      avgRating: 3.8,
+      veg: true,
+      parentId: "432565",
+      avgRatingString: "3.8",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 50,
+        lastMileTravel: 8,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "8.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-05-09 01:30:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-963d3769-ee32-4929-a9b2-48ab368f5d0d",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/cp2-0-shradha-puri-rd-meerut-cantt-meerut-727373",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "575263",
+      name: "Veg Daawat by Behrouz",
+      cloudinaryImageId: "2b579171cefc545ce6479e21c0016798",
+      locality: "Sector 9",
+      areaName: "Shastri Nagar",
+      costForTwo: "₹700 for two",
+      cuisines: [
+        "Biryani",
+        "North Indian",
+        "Kebabs",
+        "Mughlai",
+        "Beverages",
+        "Desserts",
+      ],
+      avgRating: 4.2,
+      veg: true,
+      parentId: "344904",
+      avgRatingString: "4.2",
+      totalRatingsString: "20+",
+      sla: {
+        deliveryTime: 57,
+        lastMileTravel: 13.8,
+        serviceability: "SERVICEABLE",
+        slaString: "55-60 mins",
+        lastMileTravelString: "13.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-05-09 03:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-963d3769-ee32-4929-a9b2-48ab368f5d0d",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/veg-daawat-by-behrouz-sector-9-shastri-nagar-meerut-575263",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "458028",
+      name: "Faasos - Wraps, Rolls & Shawarma",
+      cloudinaryImageId: "af33b81798b11deba338e94b7585d348",
+      locality: "Sector 9",
+      areaName: "Shastri Nagar",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Kebabs",
+        "Fast Food",
+        "Snacks",
+        "American",
+        "Healthy Food",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4.3,
+      parentId: "21809",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 54,
+        lastMileTravel: 13.8,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "13.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-05-09 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹125 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-963d3769-ee32-4929-a9b2-48ab368f5d0d",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/faasos-wraps-rolls-and-shawarma-sector-9-shastri-nagar-meerut-458028",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="w-full shadow-lg">
+      <div className="w-[110rem] flex justify-between items-center my-0 mx-auto">
+        <div className="logo-container">
+          <img
+            className="logo"
+            height="90"
+            width="90"
+            src="https://media.licdn.com/dms/image/C510BAQG8w_t4Zug04g/company-logo_200_200/0/1630583802200/dishdash_sg_logo?e=2147483647&v=beta&t=83RKZKoZcj881kOcoOpiiBl2XHW68Fb7CG6FGhBIkZQ"
+          />
+        </div>
+        <input
+          className="w-1/2 h-20 border rounded-lg outline-0 border-solid border-[rgb(232, 232, 232)] p-5 text-2xl shadow-md"
+          placeholder="Search for dish, restaurant or cusine...."
+        />
+        <div className="nav-items">
+          <ul className="flex gap-24 text-2xl">
+            <li className="cursor-pointer hover:text-[#fc8019] h-full">
+              Login
+            </li>
+            <li className="cursor-pointer hover:text-[#fc8019] h-full">
+              Sign Up
+            </li>
+            <li className="cursor-pointer hover:text-[#fc8019] h-full">Cart</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="w-full">
+      <div className="w-[110rem] my-0 mx-auto">
+        <div className="flex flex-wrap mt-12 gap-8">
+          {data?.map((restaurant = {}, index) => {
+            const {
+              info: {
+                cloudinaryImageId,
+                name,
+                avgRatingString,
+                cuisines,
+                costForTwo,
+                sla,
+              } = {},
+            } = restaurant;
+            return (
+              <div className="flex flex-col rounded-3xl p-4 w-[35rem] gap-3 cursor-pointer border border-transparent hover:border-[#e4e8ed] hover:border hover:shadow-md">
+                <img
+                  src={
+                    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+                    cloudinaryImageId
+                  }
+                  alt="res-logo"
+                  className="h-72 rounded-3xl object-cover"
+                />
+                <div className="flex flex-col">
+                  <div className="flex justify-between items-center mb-2">
+                    <h2 className="text-3xl font-medium">
+                      {name}
+                    </h2>
+                    <div className="text-2xl">
+                      {avgRatingString}
+                    </div>
+                  </div>
+                  <div className="flex justify-between items-center text-2xl font-light mb-2">
+                    <div className="overflow-hidden whitespace-nowrap text-ellipsis max-w-[60%]">{cuisines.join(", ")}</div>
+                    <div>{costForTwo}</div>
+                  </div>
+                  <div className="flex justify-end text-2xl">
+                    <div>{sla.slaString}</div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
